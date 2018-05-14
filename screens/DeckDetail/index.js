48bfaf9a259d6f1c2a8b9c2d93b8593a407cb4c6
Matheus Marques <marquesm91@gmail.com>
2018-05-14 19:53:34 -0300

    feat: Add DeckDetail props coming from navigation

--- screens/DeckDetail/index.js
+++ screens/DeckDetail/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import { Deck } from '../../components';
+import { Button, CenteredView, HeaderText, HeaderButton, Text } from './styles';
 import { colors } from '../../utils';
 
 class DeckDetail extends Component {
@@ -18,32 +18,20 @@ class DeckDetail extends Component {
   });
 
   render() {
+    const { name, numberOfCards } = this.props.navigation.state.params;
     return (
       <CenteredView>
-        <Text>Deck detail!</Text>
+        <Text>{name}</Text>
+        <Text>{numberOfCards}</Text>
+        <Button>
+          <Text>Start Quiz</Text>
+        </Button>
+        <Button>
+          <Text>Create New Question</Text>
+        </Button>
       </CenteredView>
     );
   }
 }
-
-const CenteredView = styled.ScrollView`
-  flex: 1;
-  padding: 10px;
-  background-color: ${colors.backgroundPrimary};
-`;
-
-const Text = styled.Text`
-  color: ${props => props.color || colors.default};
-`;
-
-const HeaderText = Text.extend`
-  font-size: 16;
-  font-weight: bold;
-`;
-
-const HeaderButton = styled.TouchableOpacity`
-  padding-left: ${props => props.right ? 10 : 0};
-  padding-right: ${props => props.left ? 10 : 0};
-`;
 
 export default DeckDetail;
